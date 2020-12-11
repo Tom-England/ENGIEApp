bb50c21e8f0ed04b94b22b0f2ed36937b376db1c
Tom England (UG) <b9029625@newcastle.ac.uk>
2020-12-11 11:02:35 +0000

    Added Files for blank pages

---
+++ ENGIE_App/App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ENGIE_App
+{
+    public partial class App : Application
+    {
+        public static NavigationPage NavigationPage { get; private set; }
+        private static RootPage rootPage;
+        public static bool MenuIsPresented
+        {
+            get
+            {
+                return rootPage.IsPresented;
+            }
+            set
+            {
+                rootPage.IsPresented = value;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            // Set Up For Nav Bar
+            var menuPage = new MenuPage();
+            NavigationPage = new NavigationPage(new MainPage());
+            rootPage = new RootPage();
+            rootPage.Master = menuPage;
+            rootPage.Detail = NavigationPage;
+            MainPage = rootPage;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        private void Expand(object sender, EventArgs e)
+        {
+            MenuIsPresented = true;
+        }
+    }
+}
