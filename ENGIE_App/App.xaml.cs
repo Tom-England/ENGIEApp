3d3a04309bb2ffed5ab62d2b27af872844cf2205
Tom England (UG) <b9029625@newcastle.ac.uk>
2020-12-17 10:52:06 +0000

    Changed startup page to login page

--- ENGIE_App/App.xaml.cs
+++ ENGIE_App/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ENGIE_App.views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -24,7 +25,7 @@ namespace ENGIE_App
             InitializeComponent();
             // Set Up For Nav Bar
             var menuPage = new MenuPage();
-            NavigationPage = new NavigationPage(new MainPage());
+            NavigationPage = new NavigationPage(new LoginPage());
             rootPage = new RootPage();
             rootPage.Master = menuPage;
             rootPage.Detail = NavigationPage;
