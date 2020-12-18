571c3a57a0de651ba0fd3ab189382e0e420aa518
bpayne312 <benpayne312@gmail.com>
2020-12-18 17:21:23 +0000

    began implementing Page Object Architecture testing.  Added BasePage as template and LoginPage as example

---
+++ XamUiTest/Pages/BasePage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+
+namespace XamUiTest
+{
+    public abstract class BasePage
+    {
+        private IAppDomainSetup app;
+        private string pageTitle;
+
+        protected BasePage(IApp app, string pageTitle)
+        {
+            App = app;
+            Title = pageTitle;
+        }
+
+        protected BasePage(IAppDomainSetup app, string pageTitle)
+        {
+            this.app = app;
+            this.pageTitle = pageTitle;
+        }
+
+        public string Title { get; }
+        protected IApp App { get; }
+
+        public virtual void WaitForPageToLoad() => App.WaitForElement(Title);
+    }
+}
---
+++ XamUiTest/Pages/LoginPage.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
+namespace XamUiTest.Pages
+{
+    public class LoginPage : BasePage
+    {
+        readonly Query FirstNameText, LastNameText, EmailText, PhoneText, LoginButton;
+
+        public LoginPage(IAppDomainSetup app, string pageTitle) : base(app, pageTitle)
+        {
+            FirstNameText = x => x.Marked("FirstName");
+            LastNameText = x => x.Marked("LastName");
+            EmailText = x => x.Marked("Email");
+            PhoneText = x => x.Marked("Phone");
+            LoginButton = x => x.Marked("Login");
+        }
+
+        public void EnterFirstName(string firstName)
+        {
+            App.Tap(FirstNameText);
+            App.EnterText(firstName);
+            App.Screenshot("First name entered");
+        }
+
+        public void EnterLastName(string lastName)
+        {
+            App.Tap(LastNameText);
+            App.EnterText(lastName);
+            App.Screenshot("Last name entered");
+        }
+
+        public void EnterEmail(string email)
+        {
+            App.Tap(EmailText);
+            App.EnterText(email);
+            App.Screenshot("Email entered");
+        }
+
+        public void EnterPhone(string phone)
+        {
+            App.Tap(PhoneText);
+            App.EnterText(phone);
+            App.Screenshot("Phone entered");
+        }
+
+        public void TapLoginButton()
+        {
+            App.Tap(LoginButton);
+            App.Screenshot("Login button tapped");
+        }
+    }
+}
