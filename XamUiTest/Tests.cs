db03ec35790401f70017a8f4c94c1a9e0d8d9576
bpayne312 <benpayne312@gmail.com>
2020-12-26 18:28:35 +0000

    Added UserDetailsValidated to check for erroneous login attempts

--- XamUiTest/Tests.cs
+++ XamUiTest/Tests.cs
@@ -43,5 +43,44 @@ namespace XamUiTest
             Assert.IsTrue(results.Any());
         }
 
+        // test for no first name
+        [TestCase("", "Bloggs", "joe@bloggs.com", "07777777777")]
+        // test for no surname
+        [TestCase("Joe", "", "joe@bloggs.com", "07777777777")]
+        // test for no email
+        [TestCase("Joe", "Bloggs", "", "07777777777")]
+        // test for invalid email
+        [TestCase("Joe", "Bloggs", "joe", "07777777777")]
+        // test for another invalid email
+        [TestCase("Joe", "Bloggs", "joe@blogg", "07777777777")]
+        // test for no phone number
+        [TestCase("Joe", "Bloggs", "joe@bloggs.com", "")]
+        // test for phone number too short
+        [TestCase("Joe", "Bloggs", "joe@bloggs.com", "079")]
+        // test for phone number with invalid characters
+        [TestCase("Joe", "Bloggs", "joe@bloggs.com", "0777abc")]
+        public void UserDetailsValidated(string firstName, string lastName, string email, string phone)
+        {
+            //Arrange
+            // done via testcases to avoid repitition
+
+            //Act
+            LoginPage.EnterFirstName(firstName);
+            LoginPage.EnterLastName(lastName);
+            LoginPage.EnterEmail(email);
+            LoginPage.EnterPhone(phone);
+            App.DismissKeyboard();
+            LoginPage.TapLoginButton();
+
+            App.WaitForElement(x => x.Marked("Continue"));
+            App.Tap(x => x.Marked("Continue"));
+            AppResult[] results = App.WaitForElement(c => c.Marked("Home"));
+            App.Screenshot("Home page shown after login");
+
+            //Assert
+            // assert false if login is successful, since all include invalid credentials
+            Assert.IsFalse(results.Any());
+        }
+
     }
 }
