3dfda846a2e47623e276522e100253926c162b1f
bpayne312 <benpayne312@gmail.com>
2020-12-27 15:11:03 +0000

    Added MainPage.cs for page object architecture testing

---
+++ XamUiTest/Pages/MainPage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+using Query = System.Func<Xamarin.UITest.Queries.AppQuery, Xamarin.UITest.Queries.AppQuery>;
+
+namespace XamUiTest.Pages
+{
+    public class MainPage : BasePage
+    {
+        Query MenuButton;
+
+        public MainPage(IApp app, string pageTitle) : base(app, pageTitle)
+        {
+            MenuButton = x => x.Marked("MenuButton");
+        }
+
+        public void TapMenuButton()
+        {
+            App.Tap(MenuButton);
+        }
+
+
+    }
+}
