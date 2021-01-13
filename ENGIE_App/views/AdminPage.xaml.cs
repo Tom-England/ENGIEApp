8568572b66241da36973eaaaff870a75a7275cdd
Tom England (UG) <b9029625@newcastle.ac.uk>
2021-01-13 10:32:16 +0000

    Added page for admin account control

---
+++ ENGIE_App/views/AdminPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ENGIE_App.views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AdminPage : ContentPage
+    {
+        public AdminPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            NavigationPage.SetHasBackButton(this, false);
+            InitializeComponent();
+        }
+    }
+}
