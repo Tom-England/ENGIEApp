759c94e743c879191aea9202d0e08607bbb2c958
b9031331 <b9031331@newcastle.ac.uk>
2021-01-26 20:40:53 +0000

    added admin login validation

--- ENGIE_App/views/AdminPage.xaml.cs
+++ ENGIE_App/views/AdminPage.xaml.cs
@@ -37,76 +37,104 @@ namespace ENGIE_App.views
             var password = EntryPassword.Text;
             bool check = false;
             var newsalt = "hvGirlXDVzdsCSrPmOdHRA==";
-            var hashedPassword = ComputeHash(Encoding.UTF8.GetBytes(password),
-                Encoding.UTF8.GetBytes(newsalt));
 
-            if (connection == null)
+            if (username == null && password == null)
             {
-                connection = dbconn.Connect_Database();
-                connection.Open();
-            }
-            try
-            {
-                Console.WriteLine("Connecting to MySQL...");
-                Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA " + hashedPassword);
-
-                // SQL code
-                MySqlCommand cmd = connection.CreateCommand();
-                cmd.CommandText = "SELECT Username, Password FROM Admin WHERE Username = @username and Password = @password";
-
-                // adds values user inputted to database
-                cmd.Parameters.AddWithValue("@username", username);
-                cmd.Parameters.AddWithValue("@password", hashedPassword);
-                cmd.ExecuteNonQuery();
-                MySqlDataReader reader = cmd.ExecuteReader();
-
-                if (reader.Read())
+                var nullResult = await DisplayAlert("Fill In All Fields", "Must enter Username and Password. Please Try Again", "Continue", "Cancel");
+                if (nullResult)
                 {
-                    reader.Close();
-                    check = true;
-                }
-                else
-                {
-                    reader.Close();
-                    check = false;
-                }
-
-                Application.Current.Properties["Firstname"] = username;
-
-                if (check)
-                {
-                    Device.BeginInvokeOnMainThread(async () =>
-                    {
-                        // takes user to home page upon succesful login 
-                        var result = await this.DisplayAlert("Congratulations", "User Succesfully logged on", "Continue", "Cancel");
-                        Application.Current.Properties["Admin"] = true;
-                        if (result)
-                        {
-                            var mainPage = new MainPage();
-                            var homePage = App.NavigationPage.Navigation.NavigationStack.First();
-                            App.NavigationPage.Navigation.InsertPageBefore(mainPage, homePage);
-                            await Navigation.PushAsync(new ENGIE_App.views.AdminOptionsPage());                        
-                            //await App.NavigationPage.PopToRootAsync(false);
-
-                        }
-                    }
-                    );
-                }
-                else
-                {
-                    await DisplayAlert("Access Denied", "username and/or password are incorrect", "Continue", "Cancel");
                     await Navigation.PushAsync(new ENGIE_App.views.AdminPage());
                 }
-
             }
-            catch (Exception ex)
+            else if (username == null)
             {
-                Console.WriteLine(ex.ToString());
+                var nullResult = await DisplayAlert("Fill In All Fields", "Must enter Username. Please Try Again", "Continue", "Cancel");
+                if (nullResult)
+                {
+                    await Navigation.PushAsync(new ENGIE_App.views.AdminPage());
+                }
             }
+            else if (password == null)
+            {
+                var nullResult = await DisplayAlert("Fill In All Fields", "Must enter Password. Please Try Again", "Continue", "Cancel");
+                if (nullResult)
+                {
+                    await Navigation.PushAsync(new ENGIE_App.views.AdminPage());
+                }
+            }
+            else
+            {
+                var hashedPassword = ComputeHash(Encoding.UTF8.GetBytes(password),
+                    Encoding.UTF8.GetBytes(newsalt));
 
-            dbconn.Close_Connection();
+                if (connection == null)
+                {
+                    connection = dbconn.Connect_Database();
+                    connection.Open();
+                }
+                try
+                {
 
+                    Console.WriteLine("Connecting to MySQL...");
+                    Console.WriteLine("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA " + hashedPassword);
 
+                    // SQL code
+                    MySqlCommand cmd = connection.CreateCommand();
+                    cmd.CommandText = "SELECT Username, Password FROM Admin WHERE Username = @username and Password = @password";
+
+                    // adds values user inputted to database
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", hashedPassword);
+                    cmd.ExecuteNonQuery();
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        reader.Close();
+                        check = true;
+                    }
+                    else
+                    {
+                        reader.Close();
+                        check = false;
+                    }
+
+                    Application.Current.Properties["Firstname"] = username;
+
+                    if (check)
+                    {
+                        Device.BeginInvokeOnMainThread(async () =>
+                        {
+                            // takes user to home page upon succesful login 
+                            var result = await this.DisplayAlert("Congratulations", "User Succesfully logged on", "Continue", "Cancel");
+                            Application.Current.Properties["Admin"] = true;
+                            if (result)
+                            {
+                                var mainPage = new MainPage();
+                                var homePage = App.NavigationPage.Navigation.NavigationStack.First();
+                                App.NavigationPage.Navigation.InsertPageBefore(mainPage, homePage);
+                                await Navigation.PushAsync(new ENGIE_App.views.AdminOptionsPage());
+                                //await App.NavigationPage.PopToRootAsync(false);
+
+                            }
+                        }
+                        );
+                    }
+                    else
+                    {
+                        await DisplayAlert("Access Denied", "username and/or password are incorrect", "Continue", "Cancel");
+                        await Navigation.PushAsync(new ENGIE_App.views.AdminPage());
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                dbconn.Close_Connection();
+            }
+            
         }
 
     }
