8b41b38299c5578f163818b0918bd56bf16d83d4
Tom England (UG) <b9029625@newcastle.ac.uk>
2021-01-27 20:55:46 +0000

    Updated buttons on admin options page

--- ENGIE_App/views/AdminOptionsPage.xaml.cs
+++ ENGIE_App/views/AdminOptionsPage.xaml.cs
@@ -28,17 +28,34 @@ namespace ENGIE_App.views
         bool checker = false;
         EmailHelper eHelper = new EmailHelper();
 
+        bool setButtonEnable = false;
+        bool sendButtonEnable = false;
+        bool qrButtonEnable = false;
+
         public AdminOptionsPage()
         {
             NavigationPage.SetHasNavigationBar(this, false);
             NavigationPage.SetHasBackButton(this, false);
             InitializeComponent();
+            FakeDisable(setEmailBtn);
+            FakeDisable(sendEmailBtn);
+            FakeDisable(QRButton);
         }
 
         public string ComputeHash(byte[] bytesToHash, byte[] salt)
         {
             var byteResult = new Rfc2898DeriveBytes(bytesToHash, salt, 10000);
             return Convert.ToBase64String(byteResult.GetBytes(24));
+        }
+
+        private void FakeDisable(Button btn)
+        {
+            btn.TextColor = Color.FromHex("#444444");
+            
+        }
+        private void FakeEnable(Button btn)
+        {
+            btn.TextColor = Color.FromHex("#FFF");
         }
 
         private async void Button_Clicked(object sender, EventArgs e)
@@ -83,51 +100,73 @@ namespace ENGIE_App.views
 
         private void Set_Email(object sender, EventArgs e)
         {
-            eHelper.SetDes(EntryDesEmail.Text);
+            if (setButtonEnable)
+            {
+                eHelper.SetDes(EntryDesEmail.Text);
 
-            Application.Current.Properties["Email"] = eHelper.GetDes();
+                Application.Current.Properties["Email"] = eHelper.GetDes();
 
-            checker = true;
-            sendEmailBtn.IsEnabled = checker;
+                checker = true;
+                sendButtonEnable = checker;
+                qrButtonEnable = true;
+                FakeEnable(sendEmailBtn);
+                FakeEnable(QRButton);
+            }
+            else
+            {
+                DisplayAlert("Alert", "Please enter a valid email", "OK");
+            }   
         }
 
         public async void Send_Email(object sender, EventArgs e)
         {
-            eHelper.SendEmail("Test", "This is bodyText");
-            await this.DisplayAlert("Succes", "email sent", "Continue", "Cancel");
+            if (sendButtonEnable)
+            {
+                eHelper.SendEmail("Test", "This is bodyText");
+                await this.DisplayAlert("Succes", "email sent", "Continue", "Cancel");
+            }
+            else
+            {
+                DisplayAlert("Alert", "Please enter a valid email", "OK");
+            }
         }
 
         private void ValidateEmail(object sender, TextChangedEventArgs e)
         {
-            setEmailBtn.IsEnabled = Regex.IsMatch(EntryDesEmail.Text, @"^((([a-z, A-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z, A-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z, A-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z, A-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z, A-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z, A-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z, A-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z, A-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z, A-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z, A-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$");
+            setButtonEnable = Regex.IsMatch(EntryDesEmail.Text, @"^((([a-z, A-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z, A-Z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z, A-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z, A-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z, A-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z, A-Z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z, A-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z, A-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z, A-Z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z, A-Z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$");
+            if (setButtonEnable) { FakeEnable(setEmailBtn); }
         }
 
         private void GenerateQR(object sender, EventArgs e)
         {
-
-
-            var text = SetSelectedItem();
-            if (text != null)
+            if (qrButtonEnable)
             {
-                if (eHelper.GetDes() != null)
+                var text = SetSelectedItem();
+                if (text != null)
                 {
-                    var generator = new QRBuilder();
-                    var QRData = generator.CreateQRCode(text);
+                    if (eHelper.GetDes() != null)
+                    {
+                        var generator = new QRBuilder();
+                        var QRData = generator.CreateQRCode(text);
 
-                    QRLabel.Text = "Generated Successfully";
-                    //await Clipboard.SetTextAsync(QRData);
-                    Console.WriteLine(QRData);
-                    generator.SaveImage(generator.CreateImageFromText(QRData), eHelper.GetDes());
-                    //var test = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-                    //var filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "MyQR.png");
-                    //var filename = Path.Combine(test, "MyQR.png");
+                        QRLabel.Text = "Generated Successfully";
+                        //await Clipboard.SetTextAsync(QRData);
+                        Console.WriteLine(QRData);
+                        generator.SaveImage(generator.CreateImageFromText(QRData), eHelper.GetDes());
+                        //var test = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                        //var filename = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "MyQR.png");
+                        //var filename = Path.Combine(test, "MyQR.png");
+                    }
+                }
+                else
+                {
+                    QRLabel.Text = "No Item Selected";
                 }
             }
             else
             {
-                QRLabel.Text = "No Item Selected";
+                DisplayAlert("Alert", "Please set a valid email", "OK");
             }
-
 
         }
 
