11e94aefff6dd251681ae2787f17e759ac0d4b97
Tom England (UG) <b9029625@newcastle.ac.uk>
2021-01-29 14:49:48 +0000

    Update SaveAndroid.cs

--- ENGIE_App.Android/SaveAndroid.cs
+++ ENGIE_App.Android/SaveAndroid.cs
@@ -1,3 +1,10 @@
+﻿// Copyright Syncfusion Inc. 2001 - 2017. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws.
+// Modified by MO and BP 2021-01-27
+
 ﻿using System;
 using System.IO;
 using Android.Content;
